e6548547719ea81c7c1d915c005585c27ec076b5
Priyanshu <priyanshu@tamu.edu>
2022-05-23 17:46:03 -0500

    Proc

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
-    return render_template('home.html')
+    return render_template('index.html')
 
 @app.route('/aboutme.html', methods=['GET', 'POST'])
 def aboutme():
